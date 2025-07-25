b38510633693cf16848d660fcf03f0032017ee01
davidcoats <david@getsafetycone.com>
2025-07-25 12:24:19 -0700

    Interim commit.

--- source/R5T.F0120/Code/Instances.cs
+++ source/R5T.F0120/Code/Instances.cs
@@ -9,7 +9,7 @@ namespace R5T.F0120
         public static IDateTimeFormats DateTimeFormats => F0120.DateTimeFormats.Instance;
         public static IDateTimeFormatTemplates DateTimeFormatTemplates => F0120.DateTimeFormatTemplates.Instance;
         public static IExecutableFilePathOperator ExecutableFilePathOperator => F0120.ExecutableFilePathOperator.Instance;
-        public static F0000.IGuidOperator GuidOperator => F0000.GuidOperator.Instance;
+        public static L0066.IGuidOperator GuidOperator => L0066.GuidOperator.Instance;
         public static F0000.INowOperator NowOperator => F0000.NowOperator.Instance;
         public static IStringOperator StringOperator => F0120.StringOperator.Instance;
         public static Z0000.IStrings Strings => Z0000.Strings.Instance;
